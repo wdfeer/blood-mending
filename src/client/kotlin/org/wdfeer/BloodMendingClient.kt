274b3ab1acc936861ab2d6ac547ecde081729a3b
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-16 11:54:24 +0200

    Remove redundant entrypoints

--- src/client/kotlin/org/wdfeer/BloodMendingClient.kt
+++
@@ -1,9 +0,0 @@
-package org.wdfeer
-
-import net.fabricmc.api.ClientModInitializer
-
-object BloodMendingClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
